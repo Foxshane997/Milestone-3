ed72133af9e4856a6fedd570c70a000854e4718a
“Foxshane997” <Foxshane997@gmail.com>
2024-09-24 19:33:47 -0700

    added bcrypt to user creation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,10 +1,19 @@
 const db = require('../config/db');
+const bcrypt = require('bcrypt')
+
+const saltRounds = 10;
 
 const User = {
     createUser: async (username, password, admin = false) => {
-        const query = 'INSERT INTO users (username, password, admin) VALUES ($1, $2, $3) RETURNING *';
-        const result = await db.query(query, [username, password, admin]);
-        return result.rows[0];
+        try {
+
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            const query = 'INSERT INTO users (username, password, admin) VALUES ($1, $2, $3) RETURNING *';
+            const result = await db.query(query, [username, hashedPassword, admin]);
+            return result.rows[0];
+        } catch (error) {
+            throw new Error('Error creating user: ' + error.message);
+        }
     },
 
     getAllUsers: async () => {
