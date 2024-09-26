121fe2097a46ae8229dd659b0418d36b1cb45e3c
Nicholas Mendoza <155332543+GANTZ-Online@users.noreply.github.com>
2024-09-25 19:33:26 -0700

    login backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const { createUser, getAllUsers } = require('../controllers/userController');
+const { loginUser } = require('../controllers/loginPage'); // Import the login controller
+
 const router = express.Router();
 
 // Define routes
 router.post('/', createUser); 
 router.get('/', getAllUsers);
+router.post('/login', loginUser); // Use the loginUser controller for the login route
 
 module.exports = router;
