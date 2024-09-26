659763663ae01adbb841fc0ffb423b61c404ba6b
“Foxshane997” <Foxshane997@gmail.com>
2024-09-25 18:46:02 -0700

    adding cors for front end connection

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,17 @@
-// const express = require('express');
-// const app = express();
-// require('dotenv').config()
-
-// const port = process.env.PORT
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}`);
-// });
-
-
-// backend/server.js
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 // Middleware
 app.use(bodyParser.json()); 
