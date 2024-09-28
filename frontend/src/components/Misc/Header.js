d0783a15e81fa44225e3a8360796a5775455756b
“Foxshane997” <Foxshane997@gmail.com>
2024-09-28 14:14:37 -0700

    adding to repo

--- frontend/src/components/Misc/Header.js
+++ frontend/src/components/Misc/Header.js
@@ -6,6 +6,7 @@ const Header = ({ user, handleLogout }) => {
             {user ? (
                 <>
                     <p>Logged in as: {user.username}</p>
+                    {user.isAdmin && <p>You are an admin.</p>}
                     <button onClick={handleLogout}>Logout</button>
                 </>
             ) : (
