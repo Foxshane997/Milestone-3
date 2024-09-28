9980bbdc683e514dcd4460b8bcce7542ea8415ce
“Foxshane997” <Foxshane997@gmail.com>
2024-09-28 14:20:38 -0700

    needs to change model getAll table name query

--- backend/models/songQueue.js
+++ backend/models/songQueue.js
@@ -2,13 +2,13 @@ const db = require('../config/db');
 
 class Song {
     static async createSong({ username, name }) {
-        const timestamp = new Date(); // Get the current timestamp
+        const timestamp = new Date();
         try {
             const result = await db.query(
                 'INSERT INTO song_queue (username, name, time_submitted) VALUES ($1, $2, $3) RETURNING *',
                 [username, name, timestamp]
             );
-            return result.rows[0]; // Return the newly created song
+            return result.rows[0];
         } catch (error) {
             throw new Error('Database Error: ' + error.message);
         }
@@ -16,7 +16,7 @@ class Song {
 
     static async getAllSongs() {
         try {
-            const result = await db.query('SELECT * FROM songs ORDER BY time_submitted DESC');
+            const result = await db.query('SELECT * FROM song_queue ORDER BY time_submitted DESC');
             return result.rows;
         } catch (error) {
             throw new Error('Database Error: ' + error.message);
